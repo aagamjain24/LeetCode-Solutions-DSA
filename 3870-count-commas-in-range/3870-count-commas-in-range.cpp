8d8c9bd55789b354572fbb76b3a3a55da8204a38
Aagam Jain <176695409+aagamjain24@users.noreply.github.com>
2026-09-09 00:08:48 +0530

    Time: 8 ms (8.28%), Space: 8.6 MB (15.51%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+
+        for (long long i = 1000; i <= n; i++) {
+            if (i >= 1000) ans++;
+            if (i >= 1000000) ans++;
+            if (i >= 1000000000) ans++;
+            if (i >= 1000000000000LL) ans++;
+        }
+
+        return ans;
+    }
+};
